aea1409e81d5a4ef5525a93f1b770bde2ecf54c0
valeska <valeskaaparicio5@gmail.com>
2026-08-12 20:49:59 -0600

    estructura inicial y variables

---
+++ src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -0,0 +1,19 @@
+package com.mycompany.tarea2;
+
+import java.util.Scanner;
+
+public class Tarea2 {
+    /*
+     * Programa para práctica de versionamiento y Java
+     * (lectura de datos, validación y arreglos)
+     */
+    public static void main(String[] args) {
+        // Declaración e inicialización de 4 datos primitivos
+        int limiteElementos = 5;
+        double promedioGeneral = 0.0;
+        char identificador = 'A';
+        boolean esProgramaActivo = true;
+
+        System.out.println("=== PROGRAMA DE CONSOLA INICIADO ===");
+    }
+}
